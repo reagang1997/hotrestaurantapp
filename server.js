d1b13338d57da03ee5b6b05d87bc59e44b4f2bc8
ReaganG <rbgrunwald1997@gmail.com>
2021-02-01 11:20:03 -0600

    port fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
